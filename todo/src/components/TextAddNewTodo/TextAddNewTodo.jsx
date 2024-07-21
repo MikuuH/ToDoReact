b3b54e047af33008b4301a89891f8ded031e044f
miku <39mkui@gmail.com>
2024-07-21 17:46:27 +0300

    -

--- todo/src/components/TextAddNewTodo/TextAddNewTodo.jsx
+++ todo/src/components/TextAddNewTodo/TextAddNewTodo.jsx
@@ -1,14 +1,26 @@
 import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
 
-export default function TextAddNewTodo() {
+export default function TextAddNewTodo({ newTodo, setNewTodo, handleAddTodo }) {
   return (
     <div className="flex items-center justify-center">
-      <div className="w-[50%] mt-5">
+      <div className="w-[50%] mt-5 flex">
         <TextField
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
           fullWidth
           label="Новая задача"
           id="fullWidth"
+          variant="outlined"
         />
+        <Button
+          onClick={handleAddTodo}
+          variant="contained"
+          color="primary"
+          className="ml-2"
+        >
+          Добавить
+        </Button>
       </div>
     </div>
   );
--- todo/src/components/ToDos/ToDos.jsx
+++ todo/src/components/ToDos/ToDos.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 import TextAddNewTodo from "../TextAddNewTodo/TextAddNewTodo";
 import Body from "../Body/Body";
 
 export default function ToDos() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = () => {
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo]);
+      setNewTodo("");
+    }
+  };
+
   return (
     <Body>
-        <div>
-            <TextAddNewTodo />
-            
-        </div>
+      <div className="flex flex-col items-center">
+        <TextAddNewTodo newTodo={newTodo} setNewTodo={setNewTodo} handleAddTodo={handleAddTodo} />
+        <ul className="mt-4">
+          {todos.map((todo, index) => (
+            <li key={index} className="border-b border-gray-300 py-2">
+              {todo}
+            </li>
+          ))}
+        </ul>
+      </div>
     </Body>
-
   );
 }
